7cabbfad120d3ae0eea1f9b8856590fb3bc8fcb9
akshaya-arignar <akshaya@arignar.app>
2025-09-18 18:40:57 +0530

    initial commit

---
+++ Backend/server/server.js
@@ -0,0 +1,33 @@
+// server/server.js
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// basic root
+app.get("/", (req, res) => res.send("Kids Tamil Learning API running"));
+
+// simple DB status endpoint
+app.get("/api/dbstatus", (req, res) => {
+  const states = ["disconnected","connected","connecting","disconnecting"];
+  res.json({ mongoState: states[mongoose.connection.readyState] });
+});
+
+// connect DB then start server
+const PORT = process.env.PORT || 5000;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("Could not start server because DB failed to connect.");
+  });
